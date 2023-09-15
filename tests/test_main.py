2fa809cf4f5d80c5d6745dc15e16d7228f8c1c69
Perran Thomas <perran.thomas@icloud.com>
2023-09-15 10:59:50 +0100

    Wrote tests for Phase One and rewrote code for Phase One.

---
+++ tests/test_main.py
@@ -0,0 +1,48 @@
+# pylint: skip-file
+
+from lib.main import *
+
+# Phase One
+# General concepts of cards, players and hands:
+#     Cards (Card class and 52 instances thereof?) can be shuffled and dealt to 4 Player objects
+#     Concept of a "hand" is created for a Player, with some way to print that in an appropriate fashion to the terminal
+
+def test_create_players():
+    game = Game()
+    game.create_players()
+    assert len(game.players) == 4
+    assert all(isinstance(player, Player) for player in game.players)
+
+def test_set_human_player():
+    game_with_human = Game()
+    game_with_human.create_players(human_playing = True)
+    assert [
+        player.is_human for player in game_with_human.players
+    ] == [True, False, False, False]
+
+def test_only_bots_created():
+    bots_only_game = Game()
+    bots_only_game.create_players(human_playing = False)
+    assert all(not player.is_human for player in bots_only_game.players)
+
+def test_full_deck_created():
+    deck = Card.make_fresh_deck()
+    assert len(deck) == 52
+    assert all(isinstance(card, Card) for card in deck)
+
+def test_deal_cards():
+    game = Game()
+    game.create_players()
+    game.deal_cards()
+    assert all(len(player.hand) == 13 for player in game.players)
+    assert all(
+        all(isinstance(card, Card) for card in player.hand)
+        for player in game.players
+    )
+
+def test_report_cards():
+    game = Game()
+    game.create_players()
+    game.deal_cards()
+    report = game.players[0].report_cards_in_hand()
+    assert len(report.split(", ")) == 13
